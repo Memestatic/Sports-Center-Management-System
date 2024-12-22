72089b4c906c8f950be4f2abc72ff9ff138ff3a1
Karol Ziaja <kziaja34@gmail.com>
2024-12-22 17:45:27 +0100

    register works

--- ProjectIO/Pages/Login.cshtml.cs
+++ ProjectIO/Pages/Login.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectIO.model;
 
 namespace ProjectIO.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly SportCenterContext _context;
+
+        public LoginModel(SportCenterContext context)
+        {
+            _context = context;
+        }
+
+        // Bindowanie danych bezpoœrednio do encji User
+        [BindProperty]
+        public User Input { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); // Jeœli model jest niepoprawny, wróæ do formularza
+            }
+
+            // Dodanie u¿ytkownika do bazy danych
+            _context.Users.Add(Input);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/"); // Przekierowanie po sukcesie
         }
     }
 }
--- ProjectIO/Pages/Register.cshtml.cs
+++ ProjectIO/Pages/Register.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectIO.model;
 
 namespace ProjectIO.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly SportCenterContext _context;
+
+        public RegisterModel(SportCenterContext context)
+        {
+            _context = context;
+        }
+
+        // Bindowanie danych bezpoœrednio do encji User
+        [BindProperty]
+        public User Input { get; set; }
+
+
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); // Jeœli model jest niepoprawny, wróæ do formularza
+            }
+
+            // Dodanie u¿ytkownika do bazy danych
+            _context.Users.Add(Input);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/"); // Przekierowanie po sukcesie
+        }
+
     }
 }
