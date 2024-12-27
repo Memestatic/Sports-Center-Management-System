1205630f1674060ccb5915313aa3f32bb5d051b4
michal.walus <mw306757@student.polsl.pl>
2024-12-27 15:46:33 +0100

    add-better-payment

--- ProjectIO/Pages/Payment.cshtml.cs
+++ ProjectIO/Pages/Payment.cshtml.cs
@@ -1,14 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectIO.model;
 
 namespace ProjectIO.Pages
 {
     public class PaymentModel : PageModel
     {
         public int? reservationId;
+
+        [BindProperty]
+        public Reservation reservation { get; set; }
+
+        private readonly SportCenterContext _context;
+
+        public PaymentModel(SportCenterContext context)
+        {
+            _context = context;
+        }
         public void OnGet(int? reservationId)
         {
             this.reservationId = reservationId;
+
+            reservation = _context.Reservations.FirstOrDefault(r => r.reservationId == reservationId);
+
+        }
+
+        [IgnoreAntiforgeryToken]
+        public IActionResult OnPostUpdateStatus(int reservationId)
+        {
+            var reservation = _context.Reservations.FirstOrDefault(r => r.reservationId == reservationId);
+            if (reservation == null)
+            {
+                return NotFound("Rezerwacja nie znaleziona");
+            }
+
+            if(reservation.reservationStatus != ReservationStatus.Pending)
+            {
+                return NotFound("Rezerwacja nie jest w stanie oczekuj¹cym");
+            }
+
+            reservation.reservationStatus = ReservationStatus.Approved;
+
+            List <Reservation> toCancel = _context.Reservations
+                .Where(r => r.reservationDate == reservation.reservationDate 
+                && r.facility.facilityId == reservation.facility.facilityId)
+                .ToList();
+
+            _context.SaveChanges();
+
+            return RedirectToPage("/UserReservationManager");
         }
     }
 }
---
+++ ProjectIO/Pages/UserReservationManager.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Serialization;
+using ProjectIO.model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ProjectIO.Pages
+{
+    public class UserReservationManagerModel : PageModel
+    {
+        [BindProperty]
+        public User currentUser { get; set; }
+
+        [BindProperty]
+        public List<Reservation> reservations { get; set; }
+
+        private readonly SportCenterContext _context;
+
+        public UserReservationManagerModel(SportCenterContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            currentUser = (User)CurrentPerson.GetInstance();
+
+            reservations = _context.Reservations
+                .Include(r => r.facility) // £aduje obiekt facility
+                .ThenInclude(f => f.sportsCenter) // £aduje obiekt sportsCenter wewn¹trz facility
+                .Include(r => r.user) // £aduje obiekt user
+                .Where(r => r.user.userId == currentUser.userId)
+                .ToList();
+
+            Console.WriteLine(reservations);
+        }
+    }
+}
