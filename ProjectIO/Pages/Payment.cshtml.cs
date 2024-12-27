261a13e3762d01dd7815b0cf5ba82ab5e482bfe4
michal.walus <mw306757@student.polsl.pl>
2024-12-27 16:30:51 +0100

    add-back-denying-of-reservations

--- ProjectIO/Pages/Payment.cshtml.cs
+++ ProjectIO/Pages/Payment.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using ProjectIO.model;
 
 namespace ProjectIO.Pages
 {
     public class PaymentModel : PageModel
     {
-        public int? reservationId;
+        [BindProperty]
+        public int reservationId { get; set; }
 
         [BindProperty]
         public Reservation reservation { get; set; }
@@ -17,15 +19,50 @@ namespace ProjectIO.Pages
         {
             _context = context;
         }
-        public void OnGet(int? reservationId)
+        public void OnGet(int reservationId)
         {
+            //this.reservationId. = reservationId;
             this.reservationId = reservationId;
-
             reservation = _context.Reservations.FirstOrDefault(r => r.reservationId == reservationId);
+
 
         }
 
-        [IgnoreAntiforgeryToken]
+        public IActionResult OnPost()
+        {
+            var reservation = _context.Reservations
+                    .Include(r => r.facility) 
+                    .FirstOrDefault(r => r.reservationId == reservationId);
+
+            if (reservation == null)
+            {
+                return NotFound("Rezerwacja nie znaleziona");
+            }
+
+            if (reservation.reservationStatus != ReservationStatus.Pending)
+            {
+                return NotFound("Rezerwacja nie jest w stanie oczekuj¹cym");
+            }
+
+            reservation.reservationStatus = ReservationStatus.Approved;
+
+            List<Reservation> toCancel = _context.Reservations
+                .Where(r => r.reservationDate == reservation.reservationDate
+                && r.facility.facilityId == reservation.facility.facilityId
+                && r.reservationId != reservationId)
+                .ToList();
+
+            foreach(var r in toCancel)
+            {
+                r.reservationStatus = ReservationStatus.Denied;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToPage("/UserReservationManager");
+        }
+
+        /*[IgnoreAntiforgeryToken]
         public IActionResult OnPostUpdateStatus(int reservationId)
         {
             var reservation = _context.Reservations.FirstOrDefault(r => r.reservationId == reservationId);
@@ -49,6 +86,6 @@ namespace ProjectIO.Pages
             _context.SaveChanges();
 
             return RedirectToPage("/UserReservationManager");
-        }
+        }*/
     }
 }
