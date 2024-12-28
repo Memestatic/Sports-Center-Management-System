92aca27f23328abb5f87e19082243b7e260f4dad
Patryk Czakon <patrykczakon.szyb@gmail.com>
2024-12-28 17:37:27 +0100

    add adminpanel functionality

--- ProjectIO/Pages/AdminPanel.cshtml.cs
+++ ProjectIO/Pages/AdminPanel.cshtml.cs
@@ -11,16 +11,24 @@ namespace ProjectIO.Pages
 {
     public class AdminPanel : PageModel
     {
-        private readonly SportCenterContext _context;
+
+        public List<SportsCenter> SportsCenters { get; set; }
+        public List<Facility> Facilities { get; set; }
+        public List<User> Users { get; set; }
+        public List<Worker> Workers { get; set; }
+        public List<Reservation> Reservations { get; set; }
+
+        public string ActiveTab { get; set; }
         public int Permissions { get; set; }
+
+        private readonly SportCenterContext _context;
         public AdminPanel(SportCenterContext context)
         {
             _context = context;
         }
 
-        public IActionResult OnGet()
+        public IActionResult OnGet(string tab)
         {
-
             if (CurrentPerson.GetInstance() == null)
             {
                 return RedirectToPage("/WorkLogin");
@@ -34,10 +42,99 @@ namespace ProjectIO.Pages
                .FirstOrDefault();
 
 
-            Console.WriteLine(Permissions);
+            SportsCenters = _context.SportsCenters.ToList();
+            Facilities = _context.Facilities.ToList();
+            Users = _context.Users.ToList();
+            Workers = _context.Workers.ToList();
+            Reservations = _context.Reservations.ToList();
+
+            if (!string.IsNullOrEmpty(tab))
+            {
+                ActiveTab = tab;
+            }
+
+
+                // Obs³uga ró¿nych zak³adek
+                switch (tab)
+            {
+                case "tab1":
+                    SportsCenters = _context.SportsCenters.ToList(); // Przyk³ad dla zak³adki 1
+                    break;
+                case "tab2":
+                    Facilities = _context.Facilities.ToList(); // Przyk³ad dla zak³adki 2
+                    break;
+                case "tab3":
+                    Users = _context.Users.ToList();
+                    break;
+                case "tab4":
+                    Workers = _context.Workers.ToList();
+                    break;
+                case "tab5":
+
+                    break;
+                case "tab6":
+                    Facilities = _context.Facilities.ToList();
+                    break;
+                case "tab7":
+                    Reservations = _context.Reservations.ToList();
+                    break;
+                // Dodaj wiêcej przypadków dla pozosta³ych zak³adek
+                default:
+                   // SportsCenters = _context.SportsCenters.ToList(); 
+                   // Facilities = _context.Facilities.ToList();
+                   // Users = _context.Users.ToList();
+                   // Workers = _context.Workers.ToList();
+                   // Reservations = _context.Reservations.ToList();
+                    break;
+            }
 
             return Page();
-            //}
         }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var sc = _context.SportsCenters.Find(id);
+            if (sc != null)
+            {
+                _context.SportsCenters.Remove(sc);
+                _context.SaveChanges();
+            }
+            return RedirectToPage(); // Przekierowanie po usuniêciu
+        }
+
+        public IActionResult OnPostEdit(int id, string centerName, string centerStreet, string centerStreetNumber, string centerCity, string centerState, string centerZip)
+        {
+            var sc = _context.SportsCenters.Find(id);
+            if (sc != null)
+            {
+                sc.centerName = centerName;
+                sc.centerStreet = centerStreet;
+                sc.centerStreetNumber = centerStreetNumber;
+                sc.centerCity = centerCity;
+                sc.centerState = centerState;
+                sc.centerZip = centerZip;
+                _context.SaveChanges();
+            }
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostAdd(string centerName, string centerStreet, string centerStreetNumber, string centerCity, string centerState, string centerZip)
+        {
+            var sc = new SportsCenter
+            {
+                centerName = centerName,
+                centerStreet = centerStreet,
+                centerStreetNumber = centerStreetNumber,
+                centerCity = centerCity,
+                centerState = centerState,
+                centerZip = centerZip
+            };
+
+            _context.SportsCenters.Add(sc);
+            _context.SaveChanges();
+            return RedirectToPage();
+        }
+
     }
 }
