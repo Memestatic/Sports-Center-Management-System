10ac029494d68097074f02ca1a97178d1b95a0ad
Patryk Czakon <patrykczakon.szyb@gmail.com>
2024-12-30 17:40:27 +0100

    working user/worker tabs in adminpanel

--- ProjectIO/Pages/WorkLogin.cshtml.cs
+++ ProjectIO/Pages/WorkLogin.cshtml.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjectIO.model;
-using ProjectIO.model;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
