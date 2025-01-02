5b5575b36f0fd2e68b68a64586726859b6377e73
Karol Ziaja <kziaja34@gmail.com>
2025-01-02 16:57:49 +0100

    hashed all passwords
    needs adding new admin in order to manage anything

--- ProjectIO/Pages/WorkLogin.cshtml.cs
+++ ProjectIO/Pages/WorkLogin.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjectIO.model;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProjectIO.Pages
 {
@@ -36,20 +38,52 @@ namespace ProjectIO.Pages
             }
 
             // Wyszukiwanie u¿ytkownika w bazie danych
-            var user = _context.Workers
-                .FirstOrDefault(w => w.email == Input.email && w.password == Input.password);
+            var worker = _context.Workers
+                .FirstOrDefault(w => w.email == Input.email);
 
-            if (user == null)
+            if (worker == null)
             {
-                // Jeœli u¿ytkownik nie zosta³ znaleziony
-                ErrorMessage = "Invalid email or password.";
+                /// !!! UWAGA - ODKOMENTOWAÆ ABY DODAÆ PIERWSZEGO ADMINISTRATORA !!! ///
+                /// 
+
+                //// Jeœli u¿ytkownik nie zosta³ znaleziony, dodaj nowego pracownika
+                //var passwordHasher = new PasswordHasher<Worker>();
+
+                //var functionId = _context.WorkerFunctions.FirstOrDefault(fc => fc.functionId == 1);
+
+                //worker = new Worker
+                //{
+                //    function = functionId,
+                //    email = Input.email,
+                //    name = "John", // WprowadŸ domyœln¹ wartoœæ lub pobierz z formularza
+                //    surname = "Doe", // WprowadŸ domyœln¹ wartoœæ lub pobierz z formularza
+                //    gender = Gender.Male, // Domyœlna wartoœæ dla p³ci, jeœli brak w formularzu
+                //    phone = "123456789", // Domyœlny numer telefonu
+                //    password = passwordHasher.HashPassword(null, Input.password) // Hashuj has³o
+                //};
+
+                //_context.Workers.Add(worker);
+                //_context.SaveChanges();
+
+                //// Dodaj komunikat informacyjny
+                //ErrorMessage = "New worker created. Please log in again.";
                 return Page();
             }
 
-            CurrentPerson.SetInstance(user); // Ustawienie zalogowanego u¿ytkownika
+            var passwordHasherForVerification = new PasswordHasher<Worker>();
+            var result = passwordHasherForVerification.VerifyHashedPassword(worker, worker.password, Input.password);
+
+            if (result == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError("Input.password", "The password is incorrect.");
+                return Page();
+            }
+
+            CurrentPerson.SetInstance(worker); // Ustawienie zalogowanego u¿ytkownika
 
             return RedirectToPage("/AdminPanel");
         }
+
     }
 
 }
